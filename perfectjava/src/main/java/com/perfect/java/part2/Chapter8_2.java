4566c1c6f07e88e1c95dd41120a31035c1440835
wAiba8860 <wayjava03remi03@gmail.com>
2025-06-26 23:19:57 +0900

    note PC

--- perfectjava/src/main/java/com/perfect/java/part2/Chapter8_2.java
+++ perfectjava/src/main/java/com/perfect/java/part2/Chapter8_2.java
@@ -1,10 +1,19 @@
 package com.perfect.java.part2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.EnumMap;
 import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
+import java.util.Set;
 
 public class Chapter8_2 {
 
@@ -36,18 +45,341 @@ public class Chapter8_2 {
         System.out.println(set);
     }
 
-    public static void main(String[] args) {
-        setObjectExample();
+    // 変更不可コレクションへの変更
+    static void CollectionUnmodifiable() {
+        // 変更可能コレクション
+        var list = new ArrayList<String>();
+        list.add("abc");
+        list.add("def");
+
+        // 変更不可コレクションに変更
+        // 変数の型はvaeで省略可能。ここでは明示
+        List<String> uList = Collections.unmodifiableList(list);
+
+        // 変更不可コレクションに対する変更は
+        // UnsupportedOperationException実行時例外発生
+        // uList.add("xyz");
+
+        // 別解
+        // List.copyOfメソッドでも変更不可コレクションに変更かのう
+        List<String> ulist2 = List.copyOf(list);
+
+        ulist2.add("xyz");
+    }
+
+    static void collectionsCopy() {
+        // コピー元のListオブジェクト
+        var list = new ArrayList<String>();
+        list.add("abc");
+        list.add("def");
+
+        // コピー先のListオブジェクト
+        var list2 = new ArrayList<>(list);
+
+        // コピー先のListオブジェクトへの追加要素
+        list2.set(0, "ABC");
+        System.out.println(list2);
+
+        // コピー元のListオブジェクトから変更は見えない
+        System.out.println(list);
+
+        list.add("xyz");
+        System.out.println(list2);
+        System.out.println(list);
+    }
+
+    static void collectionShallowCopy() {
+        // コピー元のListオブジェクト
+        var list = new ArrayList<StringBuilder>();
+        list.add(new StringBuilder("abc"));
+        list.add(new StringBuilder("def"));
+
+        // コピー先のListオブジェクト
+        var list2 = new ArrayList<>(list);
+
+        // コピー先のListオブジェクトの要素自体を変更
+        list2.get(0).append("XYZ");
+
+        // コピー元のListオブジェクトから変更が見える
+        System.out.println(list);
+    }
+
+    static void collectionDeepCopy() {
+        // コピー元のListオブジェクト
+        var list = new ArrayList<StringBuilder>();
+        list.add(new StringBuilder("abc"));
+        list.add(new StringBuilder("def"));
+
+        // コピー先のListオブジェクトをストリーム処理で生成
+        var list2 = list.stream().map(sb -> {
+            return new StringBuilder(sb);
+        }).toList();
+
+        // コピー先のListオブジェクトの要素自体を変更
+        list2.get(0).append("XYZ");
+
+        // コピー元のListオブジェクトからは変更は見えない
+        System.out.println(list);
+    }
+
+    static void collectionInitial() {
+        // 型がわかりやすいように変数の型を明示
+        List<String> list = List.of("abc", "def", "ghi");
+
+        // リテラル以外の要素指定も可能です
+        var s = "abc";
+        var list2 = List.of(s, s, s);
+        System.out.println(list2);
+
+        // 要素追加は実行時例外発生（削除と置換も同様）
+        list.add("xyz");
+    }
+
+    static void arraysInitial() {
+        List<String> list = Arrays.asList("abc", "def", "ghi");
+
+        // 要素追加は実行時例外発生（削除も同様）
+        // list.add("xyz");
+
+        // 要素置換は可能（インプレースのソート処理可能）
+        list.set(0, "xyz");
+        System.out.println(list);
+    }
+
+    static void changeableListInitial() {
+        // もっとも記述が簡潔（内部的には要素をコピー）
+        var list = new ArrayList<>(List.of("abc", "def", "ghi"));
+
+        // 匿名クラス
+        var list2 = new ArrayList<String>() {
+            {
+                add("abc");
+                add("def");
+                add("ghi");
+            }
+        };
+
+        System.out.println(list);
+        System.out.println(list2);
+    }
+
+    static void mapSetInitial() {
+        // 変更不可Mapオブジェクト
+        Map<String, String> map = Map.of("k1", "v2", "k2", "v2");
+        System.out.println(map);
+
+        // 変更不可Mapオブジェクト
+        Map<String, String> map2 = Map.ofEntries(Map.entry("k1", "v1"), Map.entry("k2", "v2"));
+        System.out.println(map2);
+
+        // 変更不可Setオブジェクト
+        Set<String> set = Set.of("elem1", "elem2");
+        System.out.println(set);
+    }
+
+    static void changeableMapSetInitial() {
+        // もっとも記述が簡潔（内部的には要素をコピー）
+        var map = new HashMap<>(Map.of("k1", "v1", "k2", "v2"));
+
+        // 匿名クラスを活用
+        var map2 = new HashMap<String, String>() {
+            {
+                put("k1", "v1");
+                put("k2", "v2");
+            }
+        };
+
+        // もっとも記述が簡潔（内部的には要素をコピー）
+        var set = new HashSet<>(Set.of("elem1", "elem2"));
+
+        // 匿名クラスを活用
+        var set2 = new HashSet<String>() {
+            {
+                add("elem1");
+                add("elem2");
+            }
+        };
+    }
+
+    static void collectionsSortError() {
+        record Book(String title) implements Comparable<Book> {
+            @Override
+            public int compareTo(Book o) {
+                return this.title.compareTo(o.title);
+            }
+        }
+
+        var list = new ArrayList<Book>();
+        list.add(new Book("xyz"));
+        list.add(new Book("abc"));
+
+        // Comparableインターフェースを継承しないとコンパイルエラー
+        Collections.sort(list);
+    }
+
+    static void iterativeProcessingList() {
+        // 前提
+        List<Integer> list = List.of(1, 2, 3);
+        int sum = 0;
+
+        // 拡張for構文を使うパターン（ループ変数の型はvarで省略可能。下記2例も同様）
+        for (Integer n : list) {
+            sum += n;
+        }
+
+        // イテレーターを使うパターン
+        for (Iterator<Integer> it = list.iterator(); it.hasNext();) {
+            Integer n = it.next();
+            sum += n;
+        }
+
+        // インデックスでforループを回すパターン
+        for (int i = 0; i < list.size(); i++) {
+            Integer n = list.get(i);
+            sum += n;
+        }
+
+        // コレクションに対するイテレーション
+        calcSum(list);
+    }
+
+    static void iteratorSet() {
+        Set<Integer> set = new HashSet<>(Set.of(1, 2, 3));
+        int sum = 0;
+
+        // イテレーターを使うパターン
+        for (Iterator<Integer> it = set.iterator(); it.hasNext();) {
+            Integer n = it.next();
+            sum += n;
+        }
+
+        // 拡張for構文を使うパターン
+        for (Integer n : set) {
+            sum += n;
+        }
+
+        // コレクションに対するイテレーション
+        calcSum(set);
+    }
+
+    static int calcSum(Collection<Integer> coll) { // 引数にListでもSetでも受け入れ可能なメソッド
+        int sum = 0;
+        for (Integer n : coll) {
+            sum += n;
+        }
+        return sum;
+    }
+
+    static void iteratorMap() {
+        Map<String, String> map = new HashMap<>(Map.of("k1", "v1", "k2", "v2", "k3", "v3", "k4", "v4"));
+
+        // 拡張for構文による、キーに対する繰り返し処理
+        for (String key : map.keySet()) {
+            System.out.println(key);
+        }
+
+        // イテレーターによる、キーに対する繰り返し処理
+        for (Iterator<String> it = map.keySet().iterator(); it.hasNext();) {
+            String key = it.next();
+            System.out.println(key);
+        }
+
+        // 拡張for構文による、値に対する繰り返し処理
+        for (String val : map.values()) {
+            System.out.println(val);
+        }
+
+        // イテレーターによる、値に対する繰り返し処理
+        for (Iterator<String> it = map.values().iterator(); it.hasNext();) {
+            String val = it.next();
+            System.out.println(val);
+        }
+
+        // 拡張for構文による、キーと値に対する繰り返し処理
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+
+        // イテレーターによる、キーと値に対する繰り返し処理
+        for (Iterator<Map.Entry<String, String>> it = map.entrySet().iterator(); it.hasNext();) {
+            Map.Entry<String, String> entry = it.next();
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+    }
+
+    static void hasPreviousList() {
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
+        for (ListIterator<Integer> it = list.listIterator(list.size()); it.hasPrevious();) {
+            Integer i = it.previous();
+            System.out.println(i);
+        }
+    }
+
+    static void pauseListIterator() {
+        List<String> list = List.of("no", "no", "pause", "pauseNext", "pauseNextNext");
+        ListIterator<String> saveIt = null;
+        for (ListIterator<String> it = list.listIterator(); it.hasNext();) {
+            String s = it.next();
+            if (s.equals("pause")) {
+                saveIt = it; // 途中のイテレーターを代入
+                break;
+            }
+        }
+
+        if (saveIt != null) {
+            for (ListIterator<String> it = saveIt; it.hasNext();) {
+                String s = it.next();
+                System.out.println(s);
+            }
+        }
+    }
+
+    static void collectionIteratorError() {
+        List<String> list = new ArrayList<>(List.of("abc", "def", "ghi"));
+        // ConcurrentModificationException実行時例外が発生
+        // for (String s : list) {
+        // list.remove(0);
+        // }
+
+        // 例外は起きないが期待通りに動作しない
+        for (int i = 0; i < list.size(); i++) {
+            list.remove(i);
+        }
+        System.out.println(list);
+    }
+
+    static void collectionIteratorIncreaseDecrease() {
+        // コレクションのイテレーション中にremoveメソッドで要素を削除する
+        var list = new ArrayList<>(List.of("abc", "def", "ghi"));
+
+        // 下記コードで全要素を削除可能
+        for (Iterator<String> it = list.iterator(); it.hasNext();) {
+            it.next();
+            it.remove();
+        }
+        System.out.println(list);
+
+        // 要素の挿入処理が多い場合、ArrayListよりLinkedListのほうが効率的
+        var linkList = new LinkedList<>(List.of("abc", "def", "ghi"));
+        for (ListIterator<String> it = linkList.listIterator(); it.hasNext();) {
+            it.next();
+            it.add("XYZ");
+        }
+        System.out.println(linkList);
+    }
+
+    public static void main(String... args) {
+        collectionIteratorIncreaseDecrease();
     }
 }
-
 
 // COLUMN
 // EnumSetとEnumMap
 class EnumMain {
     // DayOfWeek
-    private static final EnumSet<DayOfWeek> holidays =
-            EnumSet.of(DayOfWeek.SUNDAY, DayOfWeek.SATURDAY);
+    private static final EnumSet<DayOfWeek> holidays = EnumSet.of(DayOfWeek.SUNDAY, DayOfWeek.SATURDAY);
 
     // EnumMapを利用して各曜日が休日かどうかをマッピング
     private static final Map<DayOfWeek, Boolean> holidaysMap = new EnumMap<>(DayOfWeek.class);
@@ -85,21 +417,5 @@ class EnumMain {
         } else {
             System.out.println("workday");
         }
-    }
-
-    // 変更不可コレクションへの変更
-    static void CollectionUnmodifiable() {
-        // 変更可能コレクション
-        var list = new ArrayList<String>();
-        list.add("abc");
-        list.add("def");
-
-        // 変更不可コレクションに変更
-    }
-
-
-    public static void main(String... args) {
-        executeByDay(DayOfWeek.FRIDAY);
-        executeByDayMap(DayOfWeek.SATURDAY);
     }
 }
---
+++ perfectjava/src/main/java/com/perfect/java/part2/Chapter8_3.java
@@ -0,0 +1,144 @@
+package com.perfect.java.part2;
+
+import java.util.Arrays;
+
+public class Chapter8_3 {
+
+    static void arrayExample() {
+        int[] arr = new int[3];
+        arr[0] = 10;
+        arr[1] = 11;
+        arr[2] = 12;
+        System.out.println(Arrays.toString(arr));
+
+        // 下記の記述も可能
+        // C言語の気泡との親和性のための記法です。使用は非推奨。
+        int array[] = new int[3];
+
+        // 配列の書き換えは可能
+        arr[0] = 123;
+        System.out.println(arr);
+
+        // 要素数の増減はコンパイルエラー
+        // The final field array.length cannot be assigned
+        // arr.length = 4;
+
+        // 参照型要素の配列
+        var arrSt = new String[3];
+        arrSt[0] = "foo";
+        arrSt[1] = "bar";
+        arrSt[2] = "baz";
+    }
+
+    static void stringBuilderArray() {
+        var arr = new StringBuilder[2];
+
+        // arr[0]とarr[1]が同じオブジェクトを参照
+        arr[0] = new StringBuilder("abc");
+        arr[1] = arr[0];
+
+        // arr[0]の参照先オブジェクトを変更
+        arr[0].append("def");
+
+        // arr[1]から変更が見える
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void assignmentArray() {
+        int[] arr = new int[3];
+        arr[0] = 0;
+        // 下記はコンパイルエラー (int n = "abc"と同種のエラー)
+        // arr[1] = "abc";
+
+        // 参照型要素の配列
+        CharSequence[] arrCh = new CharSequence[3];
+
+        // 下記はCharSequence s = "abc"と同様に問題なし
+        arrCh[0] = "abc";
+
+        // 下記はCharSequence s = new StringBuilder("def")と同様に問題なし
+        arrCh[1] = new StringBuilder("def");
+    }
+
+    static void arrayInitial() {
+        int[] arr = { 0, 1, 2 }; // 配列初期化子
+
+        // 下記はコンパイルエラー
+        // Array initializer needs an explicit target-type
+        // var arrE = { 0, 1, 2 };
+
+        // 要素がStringオブジェクト
+        String[] arrSt = { "abc", "def" };
+
+        // 要素がStringBuilderオブジェクト
+        StringBuilder[] arrSb = { new StringBuilder("abc"), new StringBuilder("def") };
+
+        // 余計なカンマは無視
+        int[] arrCom = { 0, 1, 2, };
+        System.out.println(arrCom.length);
+
+        // 宣言時以外の配列の生成
+        // 配列を引数で受け取るメソッド（本体は省略）
+        // void method(int[] arr){}
+
+        // 上記メソッドの引数に初期化した配列を渡す構文
+        arrayMethod(new int[] { 0, 1, 2 });
+    }
+
+    static void arrayMethod(int[] arr) {
+    }
+
+    static void enumerationArray() {
+        int[] arr = { 10, 20, 30 };
+
+        // 拡張for構文
+        for (var value : arr) {
+            System.out.println(value);
+        }
+
+        // インデックス使用
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    static void arraySort() {
+        // 要素が基本型の配列は昇順でソート
+        int[] arr = { 10, 2, 3, 1, 5 };
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        // 要素が参照型の配列は比較処理用メソッドを渡せます
+        // 比較処理用メソッドを渡さない場合、要素のcompareToメソッドを使います
+        // 下記コードは降順のソートです
+        // ボクシング変換とアンボクシング変換が起きるので効率的ではありません
+        Integer[] arrIntegers = { 10, 2, 3, 1, 5 };
+
+        // 第2引数はラムダ式
+        Arrays.sort(arrIntegers, (n1, n2) -> {
+            return n2 - n1;
+        });
+        System.out.println(Arrays.toString(arrIntegers));
+    }
+
+    static void equivalenceArray() {
+        int[] arr1 = { 0, 1, 2 };
+        int[] arr2 = { 0, 1, 2 };
+
+        // 等値演算で要素の同値判定はできない
+        System.out.println(arr1 == arr2);
+
+        // equalsメソッドで要素の同値判定はできない
+        System.out.println(arr1.equals(arr2));
+
+        // Arrays.equalsは要素の同値判定ができる
+        System.out.println(Arrays.equals(arr1, arr2));
+
+        // Arrays.compareは要素の大小比較ができる（結果が0であれば同値）
+        System.out.println(Arrays.compare(arr1, arr2));
+    }
+
+    public static void main(String[] args) {
+        equivalenceArray();
+    }
+}
